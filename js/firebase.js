b48657e8e349011db2aa86ab2e7befdaa7d159bc
chillmane75 <108370900+chillmane75@users.noreply.github.com>
2026-01-22 19:21:18 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,8 @@
-
+const firebaseConfig = {
+    apiKey: "AIzaSyCVNXfmezAwYXPFzu7b_mBlUWCDNnVek_s",
+    authDomain: "kitchen-prep.firebaseapp.com",
+    projectId: "kitchen-prep",
+    storageBucket: "kitchen-prep.firebasestorage.app",
+    messagingSenderId: "986168499689",
+    appId: "1:986168499689:web:8133e23a6012c957e87f78"
+  };
