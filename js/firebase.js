06577840e33cd82d3082358a2c5753e252d8d894
chillmane75 <108370900+chillmane75@users.noreply.github.com>
2026-01-22 19:25:22 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCVNXfmezAwYXPFzu7b_mBlUWCDNnVek_s",
     authDomain: "kitchen-prep.firebaseapp.com",
@@ -6,3 +9,6 @@ const firebaseConfig = {
     messagingSenderId: "986168499689",
     appId: "1:986168499689:web:8133e23a6012c957e87f78"
   };
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
