081a8c4904bb0aefd0043dddba8bee5b1af9c268
chillmane75 <108370900+chillmane75@users.noreply.github.com>
2026-01-22 19:26:01 +0100

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,1 +1,40 @@
+import { db } from "./firebase.js";
+import {
+  collection,
+  addDoc,
+  updateDoc,
+  doc,
+  onSnapshot,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+const col = collection(db, "prepTasks");
+
+export function listenTasks(cb) {
+  onSnapshot(col, snap => {
+    cb(snap.docs.map(d => ({ id: d.id, ...d.data() })));
+  });
+}
+
+export function addTask(data) {
+  return addDoc(col, {
+    ...data,
+    status: "active",
+    createdAt: serverTimestamp(),
+    completedAt: null
+  });
+}
+
+export function markDone(id) {
+  return updateDoc(doc(db, "prepTasks", id), {
+    status: "done",
+    completedAt: serverTimestamp()
+  });
+}
+
+export function undoTask(id) {
+  return updateDoc(doc(db, "prepTasks", id), {
+    status: "active",
+    completedAt: null
+  });
+}
