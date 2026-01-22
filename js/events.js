b02bc27ca2a428919945b9e1e7d5a99a8f2baf87
chillmane75 <108370900+chillmane75@users.noreply.github.com>
2026-01-22 19:26:30 +0100

    Update events.js

--- js/events.js
+++ js/events.js
@@ -1,1 +1,24 @@
+import { addTask } from "./db.js";
 
+const modal = document.getElementById("addPrepModal");
+
+openAddForm.onclick = () => modal.hidden = false;
+cancelAdd.onclick = () => modal.hidden = true;
+
+addPrepForm.onsubmit = e => {
+  e.preventDefault();
+
+  addTask({
+    title: title.value,
+    category: category.value,
+    priority: priority.value,
+    comment: comment.value
+  });
+
+  addPrepForm.reset();
+  modal.hidden = true;
+};
+
+toggleDone.onclick = () => {
+  doneList.hidden = !doneList.hidden;
+};
